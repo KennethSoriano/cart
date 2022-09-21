3e1c24fe72666b9826c169d8d8dc1a4b92bbe90d
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-21 14:38:49 -0700

    useReducer setup

---
+++ src/context.js
@@ -0,0 +1,34 @@
+import React, {useState, useContext, useReducer, useEffect} from "react";
+import App from "./App";
+import cartItems from './data'
+import reducer from './reducer'
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
+const AppContext = React.createContext()
+
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+}
+
+const AppProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, cartItems)
+
+    return (
+        <AppContext.Provider
+            value={{
+                ...state,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider}
