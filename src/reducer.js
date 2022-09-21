3e8ef1161a60481fa99de81e6977e939ff265a22
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-21 16:43:56 -0700

    increase/decrease

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,34 @@ const reducer = (state, action) => {
         return {...state, cart: []}
     }
     if(action.type === 'REMOVE'){
-        return {...state, cart: state.cart.filter((cartItem) => cartItem.id !== action.payload)}
+        return {
+            ...state, 
+            cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+            
+        }
     }
+    if(action.type === 'INCREASE'){
+        let tempCart = state.cart.map((cartItem) => {
+            if(cartItem.id === action.payload){
+                return {...cartItem, amount: cartItem.amount + 1}
+            }
+        });
+        return {...state, cart: tempCart}
+    }
+
+
+    if(action.type === 'DECREASE'){
+        let tempCart = state.cart
+        .map((cartItem) => {
+            if(cartItem.id === action.payload){
+                return {...cartItem, amount: cartItem.amount - 1}
+            }
+        })
+        .filter((cartItem) => cartItem.amount !== 0)
+        return {...state, cart: tempCart}
+    }
+
+
     return state
 }
 
